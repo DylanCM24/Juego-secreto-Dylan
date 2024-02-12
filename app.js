2ac8a3305a1eed71b195b5afd46d18f2e377b9e5
DylanCM24 <159729672+DylanCM24@users.noreply.github.com>
2024-02-11 18:48:10 -0600

    Juego Secreto

---
+++ app.js
@@ -0,0 +1,66 @@
+//Variables.
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+
+
+//Funcion de texto.
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if(numeroUsuario === numeroSecreto){
+        asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+        if (numeroUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El numero secreto es menor!');
+        } else { 
+        asignarTextoElemento('p', 'El numero secreto es mayor!')
+        }
+        intentos++;
+        limpiarCaja()
+    }   return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto(){
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+  
+    if (listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
+    } else {
+    if(listaNumerosSorteados.includes(numeroGenerado)) {
+        return generarNumeroSecreto()
+    } else {
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }    
+
+} 
+    }
+ 
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del número secreto de Dylan!');
+    asignarTextoElemento('p', `Digite un número del 1 al ${numeroMaximo}:`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true')
+}
+
+condicionesIniciales()
